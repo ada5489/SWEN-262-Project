ddfcac2123c25972605ba0d5957d1deda2ad1097
Adesope Adebomi <adesopeadebomi@gmail.com>
2019-11-17 17:11:49 -0500

    String correction

--- src/Database/AirportDatabase.java
+++ src/Database/AirportDatabase.java
@@ -7,33 +7,33 @@ import java.util.List;
 
 public class AirportDatabase {
 
-    HashMap<String,Airport> airpotsDb;
+    HashMap<String,Airport> airportsDb;
 
     public AirportDatabase()
     {
-        airpotsDb = new HashMap <>();
+        airportsDb = new HashMap <>();
     }
 
     public void setConnection(String airportcode, String time) {
-        airpotsDb.get(airportcode).setConnectionTime(time);
+        airportsDb.get(airportcode).setConnectionTime(time);
     }
 
     public void setDelay(String airportcode, String time) {
-        airpotsDb.get(airportcode).setDelaytime(time);
+        airportsDb.get(airportcode).setDelaytime(time);
     }
 
     public void setWeather(String airportcode, String wName, String temp)
     {
-        airpotsDb.get(airportcode).addWeather(wName,temp);
+        airportsDb.get(airportcode).addWeather(wName,temp);
     }
 
     public void AddData(Airport airport) {
-         airpotsDb.put(airport.getAirportcode(), airport);
+         airportsDb.put(airport.getAirportcode(), airport);
     }
 
     public List<List<String>> ToString() {
         List <List<String>> rows = new ArrayList <>();
-        for (Airport airport : airpotsDb.values())
+        for (Airport airport : airportsDb.values())
         {
             rows.add(airport.toArray());
         }
@@ -42,6 +42,6 @@ public class AirportDatabase {
 
     public void clear()
     {
-        airpotsDb = new HashMap <>();
+        airportsDb = new HashMap <>();
     }
 }
