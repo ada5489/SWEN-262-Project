06cdfacb833b4ed04409e0ec5b51fa8f2dd50706
Evan Nolan <emn3779@g.rit.edu>
2019-11-21 16:53:21 -0500

    added the Database Manager

---
+++ src/Model/DataBaseManager.java
@@ -0,0 +1,29 @@
+package Model;
+
+import Database.AirportDatabase;
+import Database.FlightDatabase;
+
+
+public class DataBaseManager {
+
+  private AirportDatabase adb;
+  private FlightDatabase fdb;
+
+
+  public DataBaseManager(AirportDatabase abd,FlightDatabase fdb ){
+    this.adb = abd;
+    this.fdb = fdb;
+  }
+
+
+  
+  public AirportDatabase getAdb()
+  {
+    return adb;
+  }
+  public FlightDatabase getFdb()
+  {
+    return fdb;
+  }
+
+}
