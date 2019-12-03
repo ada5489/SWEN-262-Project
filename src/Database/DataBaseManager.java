fdf648262ee39dd8ce38757f3ab588a276753dd9
Evan Nolan <emn3779@g.rit.edu>
2019-12-03 00:08:59 -0500

    Need to configure this change

--- src/Database/DataBaseManager.java
+++ src/Database/DataBaseManager.java
@@ -4,8 +4,13 @@ import Database.AirportDatabase;
 import Database.FlightDatabase;
 import Model.Itinerary;
 import Model.Reservation;
+import RequestResponse.GenerateItineraries;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.IO;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import javax.imageio.IIOException;
 
 
 public class DataBaseManager {
@@ -13,13 +18,22 @@ public class DataBaseManager {
   private AirportDatabase adb;
   private FlightDatabase fdb;
   private ReservationDatabase rdb;
-  private List<Itinerary> itineraries; //this is the list of returned itineraries when prompted
+  private GenerateItineraries git;
+  private ArrayList<Itinerary> itineraries; //this is the list of returned itineraries when prompted
+  private static  CSVReader reader;
 
 
   public DataBaseManager(){
+
+    try {
+      git = reader.readCsv();
+    } catch (IOException e){
+      System.out.println("failed to load");
+    }
     this.rdb = new ReservationDatabase();
     this.adb = new AirportDatabase();
     this.fdb = new FlightDatabase();
+
   }
   public DataBaseManager(AirportDatabase abd,FlightDatabase fdb, ReservationDatabase rdb ){
     this.adb = abd;
@@ -79,6 +93,10 @@ public class DataBaseManager {
   }
   //end Reservation database methods for the facade
 
+  public ArrayList generateItineraries(String dept, String arival,int numconnnections){
+    itineraries = git.itineraries(dept,arival,numconnnections);
+    return itineraries;
+  }
 
 
 
